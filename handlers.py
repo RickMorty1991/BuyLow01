04a0319e935a0e7a36716cc097ade7fb3fa6c812
RickMorty1991 <rickwomorty@gmail.com>
2026-01-06 11:24:46 +0200

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,7 +1,31 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-from db import set_threshold
+
 from callbacks import main_menu
+from db import add_etf, set_threshold
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.bot_data["chat_id"] = update.effective_chat.id
+
+    await update.message.reply_text(
+        "📊 ETF Monitor",
+        reply_markup=main_menu()
+    )
+
+
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.args:
+        await update.message.reply_text("❗ Використання: /add TICKER")
+        return
+
+    ticker = context.args[0].upper()
+    add_etf(ticker)
+
+    await update.message.reply_text(
+        f"✅ {ticker} додано",
+        reply_markup=main_menu()
+    )
 
 
 async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
